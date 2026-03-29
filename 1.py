4dea26c7b3cd16a1f5f2ab7add61b854f0e69100
24331a05i5-tech <24331a05i5@mvgrce.edu.in>
2026-03-29 11:44:16 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+window = tk.Tk()
+window.title("Hello World bhai")
+window.geometry("300x200")
+label = tk.Label(window, text="Hello World")
+label.pack(pady=50)
+window.mainloop()
